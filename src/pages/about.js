709557973da0eca33722e3f54346cba7d6234398
Tonya <t.howard@utexas.edu>
2020-02-27 21:01:14 -0600

    Adds more filler text that has a little more substance

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,13 @@ import Layout from "../components/layout"
 
 export default () => (
     <Layout>
-        Hello this is the about page.
+        <h1>About Me</h1>
+        <p>Hello this is the about page. I'll fill it with true but filler information about myself.</p>
+        <p>Zayger was the name of my ~very first~ digimon (Zaegermon probably), and while trying to figure out a username for an art website way back then
+            I decided on. . . Zayger! I've gone by a handful of aliases online throughout the years, but this one stuck for whatever reason.</p>
+        <p>I enjoy creating fantasy character illustrations, have a love for all kinds of animals, programming, video games, 
+            eating, and researching every little thing that piques my interest.</p>
+        <p>I've started making art ever since I had something to make it with. The golden era of Neopets is what inspired me to have an interest in computers,
+            since I spent a lot of time on there trying to teach myself digital art to draw them and HTML/CSS for my pet pages. Thank you Neopets.</p>
     </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import Layout from "../components/layout"
 
 export default () => (
     <Layout>
-        <p>Hey! Sometime soon I'll probably add cards that have some art I want to show off because this is supposed to be a ~portfolio~ after all.</p>
-        <p>Still not sure about design choices. I'd like it to be somewhat minimialistic.</p>
+        <h1>Welcome</h1>
+        <p>Hey! Sometime soon I'll probably add cards that have some art I want to show off because this is supposed to be a portfolio. Honestly
+            there wont be anything extra serious on here for a while. </p>
+        <p>Still not sure about design choices. I'd like it to be somewhat minimialistic, but now I realize why tons of modern design choices have horizontal
+            navigation bars. </p>
         <p>Here is some filler text to test some stuff.</p>
         <p>Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
         <p>Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
